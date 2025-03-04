93e3bbe55b1d0701ae206a954a83b6a66a2350d8
giuliadant <giulia.dant2003@gmail.com>
2025-03-04 17:08:55 +0100

    Updated UI, close pop function and skill point incrementer added in js

--- document.js
+++ document.js
@@ -1,3 +1,21 @@
-//Code here to make pop up close when pressing start training button
+document.getElementById("closePopup").addEventListener("click", function() //popup closes when Start Training button is pressed
+{document.getElementById("Popup").style.display="none"});
+
+
+//declaring variables
+const trainButton = document.getElementById("Clicker");
+const skillDisplay = document.getElementById("Counter");
+
+let points =  0
+
+//skill points display
+skillDisplay.innertext = "Skill Points:  " + points;
+
+trainButton.addEventListener("click", function()
+{points++;skillDisplay.innerText = "Skill Points: " + points;
+})
+
+
+
 
 
